9b90bfa9dbe369c1b9837ccd21bf74a5e20b0856
Prem Reddy <premreddy1718@gmail.com>
2022-03-28 19:17:18 +0530

    Moved User to Upper Level

--- src/main/java/com/yelp/response/BusinessReviewResponse.java
+++ src/main/java/com/yelp/response/BusinessReviewResponse.java
@@ -45,24 +45,4 @@ public class BusinessReviewResponse extends BaseResponse {
         }
     }
 
-    public static class User {
-        private String name;
-        private String image_url;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getImage_url() {
-            return image_url;
-        }
-
-        public void setImage_url(String image_url) {
-            this.image_url = image_url;
-        }
-    }
 }
